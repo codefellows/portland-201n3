8b4a618a72f2c72eddd508d1c877054cf7813ab8
Mark Fitzgerald <marktfitzgerald@icloud.com>
2016-07-16 15:14:51 -0700

    Class 07 Slides and Lecture Demo

---
+++ week_3-4/class-07/lecture_demo/app.js
@@ -0,0 +1,13 @@
+var menuItems = ["Home", "Products", "Services", "Contact Us", "Locations", "Careers"];
+
+function addMenuItems() {
+  var menu = document.getElementById("main-nav");
+  for (var index = 0; index < menuItems.length; index++) {
+    var itemTextNode = document.createTextNode(menuItems[index]);
+    var menuItemNode = document.createElement("a");
+    menuItemNode.appendChild(itemTextNode);
+    menu.appendChild(menuItemNode);
+  }
+}
+
+addMenuItems();
